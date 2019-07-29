dc63698da3cac1b9ed265e90ffe41f3329f18f88
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-29 17:07:33 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  
+end
+
+class Dog 
+end
+
+fido = Dog.new 
+fido
+  
+snoopy = Dog.new
+snoopy
+
+lassie = Dog.new
+lassie
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+  
+end
+
+class Person
+end
+
+adele_goldberg = Person.new 
+adele_goldberg
+
+alan_kay = Person.new
+alan_kay
+  
